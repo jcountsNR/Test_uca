4162804817b8517ee88d4c7559f9177c0301d01b
Joseph Counts <jcounts002@gmail.com>
2022-01-28 19:19:26 -0600

    deploy

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+ git push -f git@github.com:j-counts/uca_vue.git main:gh-pages
 
 cd -
